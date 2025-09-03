925fcf3846f2af52872fdc145217817d7308fff1
cirovisk99 <ciroyamauchi@gmail.com>
2025-09-02 22:41:31 -0300

    listas de ex

---
+++ Aula_1/Lista_2_exercicios.py
@@ -0,0 +1,12 @@
+#Exercicio 1: Area de circulo
+raio = 5 
+pi = 3.14159
+area = pi * raio**2
+print(f"{area:.2f} cm²")
+
+#Exercicio 2: Converter temp fahrenheit para celsius
+temperatura_celsius = float(input("Qual a temperatura em celsius? "))
+fahrenheit = temperatura_celsius/(5/9)+32
+print(f"A temperatura {temperatura_celsius}° (celsius) corresponde a {fahrenheit} fahrenheit.")
+
+#Exercicio 3:
