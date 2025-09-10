e77588a6765190111a9591dc8a1fc74c4bd5032f
cirovisk99 <ciroyamauchi@gmail.com>
2025-09-09 21:41:32 -0300

    LISTA 3

--- Aula_1/aula_3.py
+++ Aula_1/aula_3.py
@@ -19,12 +19,66 @@ def par_impar(x) :
         print('Par')
     else:
         print('Ímpar')      
+        
+ #EXERCICIO 3: VERIFICAR SE É ANO BISSEXTO  
+def ano_bissexto(x):
+    ano = int(x)
     
+    if ano % 4 == 0 and ano % 100 != 0:
+        print('Bissexto')
+    elif ano % 100 == 0 and ano % 400 == 0 :
+        print('Bissexto')
+    else:
+        print('Não Bissexto')
 
+ #EXERCICIO 4: VERIFICAR SE É VOGAL OU CONSOANTE
+def vogal_consoante(x):
+    letra = x.upper()
+    
+    if letra in ('A','E','I','O','U'):
+        print('Vogal')
+    else: 
+        print('Consoante')
+#EXERCICIO 5: PREÇO DO PRODUTO QUANDO ACIMA DE 10 TEM 10% DESCONTO
 
+def preco(x):
+    qtd_comprada = int(x)
+    preco = 100
+    
+    if qtd_comprada >= 10 :
+        valor_total = preco*qtd_comprada*0.9
+        print(f'O valor do pedido é de R${valor_total:.2f}')
+    else:
+        valor_total = preco*qtd_comprada
+        print(f'O valor do pedido é de R${valor_total:.2f}')
+        
+#EXERCICIO 6: VERIFICAR SE A PESSOA TEM MAIS DE 16 ANOS E PODE VOTAR.
+def idade_voto(x):
+    
+    idade = int(x)
+    
+    if idade >= 16:
+        print('Pode votar')
+    else:
+        print('Não pode votar')
+        
+        
+def letra_numero(entrada):
+    
+    if entrada.isdigit() == True:
+        entrada = int(entrada)
+        print('É número')
+        type_number(entrada)        
+        par_impar(entrada)
+        ano_bissexto(entrada)
+        preco(entrada)
+        idade_voto(entrada)
+    
+    else:
+        print('É Letra')
+        vogal_consoante(entrada)
+        
 
- 
-numero = input('coloca um numero ai: ')
-type_number(numero)        
-par_impar(numero)
+entrada = input('coloca qualquer coisa ai: ')
+letra_numero(entrada)
 
